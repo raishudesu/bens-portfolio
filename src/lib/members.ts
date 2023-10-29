a3898529647c1f3287c26c70b272b337ff28a275
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-30 00:18:15 +0800

    updated members

--- src/lib/members.ts
+++ src/lib/members.ts
@@ -5,21 +5,26 @@ import Justine from "../assets/justine.jpg";
 
 export const studies = [
   {
+    title: "SITE Experience (SITEX): AI Enhanced Community Learning Platforms",
     author: "Barysh Nikov Bacaltos",
     image: Barysh,
     link: "/barysh",
   },
   {
+    title:
+      "V-Companion: A Mental Health Support Chatbot for Information Technology Students",
     author: "Ric Ryan Nolasco",
     image: Ric,
     link: "/ric",
   },
   {
+    title: "CourseCrafters: A Quest for Knowledge",
     author: "Raven Salvador",
     image: Raven,
     link: "/raven",
   },
   {
+    title: "DentaBook: A Digital Dental Appointment Solution",
     author: "Justine Elorde",
     image: Justine,
     link: "/justine",
